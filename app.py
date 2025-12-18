b255623e4ce08e20c3b5b91a1950d0d10b1d486f
Ravi Prakash Yadav <ravicomputerpoint2@gmail.com>
2025-12-18 10:10:58 +0530

    second

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 total_students = 200
+students = []
 
 @app.route('/')
 def home():
@@ -14,7 +15,20 @@ def ns():
     
 @app.route('/manage-students')
 def ms():
-    return render_template("students/manage.html")
+    return render_template("students/manage.html", students=students)
+    
+@app.post('/save-student')
+def ss():
+    if students:
+        id=max([s['id'] for s in students])
+    else:
+        id=1
+    name=request.form['name']
+    age=request.form['age']
+    course=request.form['course']
+    new_student={'id':id, 'name':name, 'age':age, 'course':course}
+    students.append(new_student)
+    return redirect('/')
     
 if __name__ == "__main__":
 	app.run(debug=True)
