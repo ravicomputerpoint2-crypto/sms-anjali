4d8922873435fcb308581db58889f700730b48da
Ravi Prakash Yadav <ravicomputerpoint2@gmail.com>
2025-12-20 09:41:10 +0530

    teachers management working fine menu will be working good next need is counting students and teachers on dashboard and show

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 total_students = 200
 students = []
 
+teachers = []
+
 @app.route('/')
 def home():
 	return render_template("index.html",ts=total_students)
@@ -13,9 +15,17 @@ def home():
 def ns():
     return render_template("students/new.html")
     
+@app.route('/new-teacher')
+def nt():
+    return render_template("teachers/newt.html")
+    
 @app.route('/manage-students')
 def ms():
     return render_template("students/manage.html", students=students)
+    
+@app.route('/manage-teachers')
+def mt():
+    return render_template("teachers/managet.html", teachers=teachers)
     
 @app.post('/save-student')
 def ss():
@@ -30,11 +40,30 @@ def ss():
     students.append(new_student)
     return redirect('/manage-students')
     
+@app.post('/save-teacher')
+def st():
+    if teachers:
+        id=max([t['id'] for t in teachers])+1
+    else:
+        id=1
+    name=request.form['name']
+    age=request.form['age']
+    subject=request.form['subject']
+    new_teacher={'id':id, 'name':name, 'age':age, 'subject':subject}
+    teachers.append(new_teacher)
+    return redirect('/manage-teachers')
+    
 @app.route('/delete-student/<int:id>')
 def ds(id):
     global students
     students=[s for s in students if s['id'] != id]
     return redirect('/manage-students')
+    
+@app.route('/delete-teacher/<int:id>')
+def dt(id):
+    global teachers
+    teachers=[t for t in teachers if t['id'] != id]
+    return redirect('/manage-teachers')
     
     
 if __name__ == "__main__":
